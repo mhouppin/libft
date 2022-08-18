2c60c33093f05d87c09dacc58f8ddb05317abea4
Morgan Houppin <morganhouppin@gmail.com>
2022-08-18 13:53:16 +0200

    Remove tabs

--- sources/stdio/ft_dgetline.c
+++ sources/stdio/ft_dgetline.c
@@ -4,62 +4,62 @@
 
 static ssize_t getline_alloc_fail(char **buffer, size_t *size)
 {
-	free(*buffer);
-	*buffer = NULL;
-	*size = 0;
-	return -1;
+    free(*buffer);
+    *buffer = NULL;
+    *size = 0;
+    return -1;
 }
 
 ssize_t ft_dgetline(int fd, char **line)
 {
-	static char *buffer = NULL;
-	static size_t size = 0;
-	ssize_t readSize;
-	char readBuf[1024];
+    static char *buffer = NULL;
+    static size_t size = 0;
+    ssize_t readSize;
+    char readBuf[1024];
 
-	if (read(fd, NULL, 0) == -1)
-		return -1;
+    if (read(fd, NULL, 0) == -1)
+        return -1;
 
-	char *tmp = ft_memchr(buffer, '\n', size);
+    char *tmp = ft_memchr(buffer, '\n', size);
 
-	while (tmp == NULL)
-	{
-		readSize = read(fd, readBuf, 1024);
+    while (tmp == NULL)
+    {
+        readSize = read(fd, readBuf, 1024);
 
-		if (readSize <= 0)
-			break ;
+        if (readSize <= 0)
+            break ;
 
-		tmp = malloc(size + (size_t)readSize);
+        tmp = malloc(size + (size_t)readSize);
 
-		if (tmp == NULL)
-			return getline_alloc_fail(&buffer, &size);
+        if (tmp == NULL)
+            return getline_alloc_fail(&buffer, &size);
 
-		ft_memcpy(tmp, buffer, size);
-		ft_memcpy(tmp + size, readBuf, (size_t)readSize);
-		free(buffer);
-		buffer = tmp;
+        ft_memcpy(tmp, buffer, size);
+        ft_memcpy(tmp + size, readBuf, (size_t)readSize);
+        free(buffer);
+        buffer = tmp;
 
-		tmp = ft_memchr(buffer + size, '\n', (size_t)readSize);
-		size += (size_t)readSize;
-	}
+        tmp = ft_memchr(buffer + size, '\n', (size_t)readSize);
+        size += (size_t)readSize;
+    }
 
-	size_t lineSize = (tmp != NULL) ? (size_t)(tmp - buffer + 1) : size;
+    size_t lineSize = (tmp != NULL) ? (size_t)(tmp - buffer + 1) : size;
 
-	if (lineSize == 0)
-	{
-		free(buffer);
-		buffer = NULL;
-		return 0;
-	}
+    if (lineSize == 0)
+    {
+        free(buffer);
+        buffer = NULL;
+        return 0;
+    }
 
-	*line = malloc(lineSize + 1);
+    *line = malloc(lineSize + 1);
 
-	if (*line == NULL)
-		return getline_alloc_fail(&buffer, &size);
+    if (*line == NULL)
+        return getline_alloc_fail(&buffer, &size);
 
-	ft_memcpy(*line, buffer, lineSize);
-	(*line)[lineSize] = '\0';
-	size -= lineSize;
-	ft_memmove(buffer, buffer + lineSize, size);
-	return (ssize_t)lineSize;
+    ft_memcpy(*line, buffer, lineSize);
+    (*line)[lineSize] = '\0';
+    size -= lineSize;
+    ft_memmove(buffer, buffer + lineSize, size);
+    return (ssize_t)lineSize;
 }
