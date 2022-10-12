7887220f2373a4d7d3d5d1f967096df5de3800d2
Morgan Houppin <morganhouppin@gmail.com>
2022-10-12 12:33:57 +0200

    Fix barray_auto_expand() behaviour

--- sources/barray/barray_auto_expand.c
+++ sources/barray/barray_auto_expand.c
@@ -3,5 +3,5 @@
 
 int barray_auto_expand(ByteArray *b)
 {
-    return barray_reserve(b, ft_maxu(2 * ft_sqrtu(b->maxSize + 1) + 1, BARRAY_MIN_BYTE_ALLOC));
+    return barray_reserve(b, b->maxSize + ft_maxu(2 * ft_sqrtu(b->maxSize + 1) + 1, BARRAY_MIN_BYTE_ALLOC));
 }
